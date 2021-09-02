4c1a5b66ab7fabc19fec76bae7af6eecda2f5d66
Fernando Rocha <fhrlobacz@gmail.com>
2021-09-02 17:21:43 -0300

    Captação de contatos do mailchimp completa

--- organizacao_tags.py
+++ organizacao_tags.py
@@ -1,6 +1,8 @@
 from mailchimp_marketing import Client
 import configuration as cfg
 import requests
+from openpyxl import Workbook
+from datetime import date
 import json
 
 chave_api = cfg.credenciais['chave_api']
@@ -17,37 +19,61 @@ def tags_para_lista(membro):
         if(membro['tags']):
                 aux=[]
                 for tag in membro['tags']:
-                        aux.append(tag['name'])
-                tags.append(aux)
+                        tags.append(tag['name'])
         else:
-                tags.append([])
-        return tags
+                tags.append('')
+        retorno = ''
+        for elemento in tags:
+                retorno += ' '+elemento.lower()
+        return traduzir_tags(retorno)
+
+def substituir_padrao(busca,tags,retorno):
+        if(tags.find(busca.lower())>0):
+                retorno += busca.upper() + ", "
+        return retorno
+
+def traduzir_tags(tags):
+        padrao = ['aluno', 'interesse','empresa','autocad','revit','sketchup','excel','design gráfico',
+                'edição de vídeo','office','produto','promob','solidworks','design jogos']
+        retorno = ''
+        for palavra in padrao:
+                retorno = substituir_padrao(palavra,tags,retorno)
+        if(len(retorno)>2):
+                retorno = retorno[:-2]
+        return retorno
 
 class Converter_lista:
+        def __init__(self):
+                resposta = requests.get(root+'lists/'+lista+'/members?count=50&offset=50',headers=parametros_membros).json()
+                membros = resposta['members']
 
-        resposta = requests.get(root+'lists/'+lista+'/members?count=500&offset=0',headers=parametros_membros).json()
-        membros = resposta['members']
+                membros_basico = [
+                        [
+                        membro['email_address'].lower() if membro['email_address'] != '' else '',
+                        membro['merge_fields']['FNAME'].lower().capitalize() if membro['merge_fields']['FNAME'] != '' else '',
+                        membro['merge_fields']['LNAME'].lower().capitalize() if membro['merge_fields']['LNAME'] != '' else '',
+                        membro['status'],
+                        tags_para_lista(membro)
+                        ] for membro in membros]
+                self.salvar_contatos(membros_basico)
 
-        membros_basico = [
-                [
-                membro['email_address'].lower() if membro['email_address'] != '' else '',
-                membro['merge_fields']['FNAME'].lower().capitalize() if membro['merge_fields']['FNAME'] != '' else '',
-                membro['merge_fields']['LNAME'].lower().capitalize() if membro['merge_fields']['LNAME'] != '' else '',
-                membro['status'],
-                tags_para_lista(membro)[0]
-                ] for membro in membros]
-
-        for membro in membros_basico:
-                dados = {
-                        'email_address':membro[0],
-                        'status': membro[3],
-                        'merge-fieds':{
-                                'FNAME':membro[1],
-                                'LNAME':membro[2]
-                        }
-                }
-                tags = membro[4]
-                print(dados)
+                # for membro in membros_basico:
+                #         dados = {
+                #                 'email_address':membro[0],
+                #                 'status': membro[3],
+                #                 'merge-fieds':{
+                #                         'FNAME':membro[1],
+                #                         'LNAME':membro[2]
+                #                 }
+                #         }
+                #         tags = membro[4]
+        
+        def salvar_contatos(self,audiencia):
+                wb = Workbook()
+                ws = wb.create_sheet('contatos',0)
+                for contato in audiencia:
+                        ws.append(contato)
+                wb.save('./ContatosMailChimp_'+str(date.today())+'.xlsx')
 
 class Obter_tags:
         def __init__(self):
@@ -76,20 +102,3 @@ class Adicionar_audiencia:
                 )
 
 #Converter_lista()
-#Obter_tags()
-
-# aluno
-# interesse
-# empresa
-# ----------
-# autocad
-# revit
-# sketchup
-# excel
-# design gráfico
-# edição de video
-# office
-# produto
-# promob
-# solidworks
-# design jogos
