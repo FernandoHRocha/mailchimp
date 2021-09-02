9a0e84a3f42f07e65039b5c3642ad7dfaf3f401e
Fernando Rocha <fhrlobacz@gmail.com>
2021-09-02 11:44:13 -0300

    Coletando dados de membros inscritos para nova inserção

--- organizacao_tags.py
+++ organizacao_tags.py
@@ -1,6 +1,21 @@
 import configuration as cfg
 import requests
+import json
 
-api = cfg.credentials['api']
-server = cfg.credentials['server']
-lista = cfg.credentials['list']
+chave_api = cfg.credentials['chave_api']
+servidor = cfg.credentials['servidor']
+lista = cfg.credentials['lista']
+root = 'https://'+servidor+'.api.mailchimp.com/3.0/'
+
+parametros_membros = {
+        'Authorization': 'apiKey '+chave_api,
+}
+
+resposta = requests.get(root+'lists/'+lista+'/members?count=5&offset=10&status=subscribed',headers=parametros_membros).json()
+membros = resposta['members']
+lista_membros = [[membro['email_address'],membro['merge_fields']['FNAME'],membro['merge_fields']['LNAME']]for membro in membros]
+#print([tag if (len(tag) > 0) else None for tag in [membro['tags'] for membro in membros]])
+#print([membro['merge_fields']['FNAME'] for membro in membros])
+#print(lista_membros[0])
+
+#response = mailchimp.lists.add_list_member(cfg.credentials['lista'], {"email_address": "", "status": "subscribed"})
